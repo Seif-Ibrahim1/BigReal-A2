dab5c5a234d2127284c666242071f0c1214015d6
ShahdMostafa30 <101884728+ShahdMostafa30@users.noreply.github.com>
2022-10-31 20:53:07 +0200

    add sign , size , > , < , == functions

--- main.cpp
+++ main.cpp
@@ -6,11 +6,19 @@ int main() {
     BigReal num3("9.");
     // BigReal num4("9.8.7"); // should print error
     BigReal num6("000055.540000000");
-    BigReal num7("34.86");
+    BigReal num7("-34.86");
+    BigReal num8("-34.86");
 
     cout<< num1.getNum() <<endl;
     cout<< num2.getNum() <<endl;
     cout<< num3.getNum() <<endl;
+
+    cout << num6.size() << endl;
+    cout << num6.sign() << endl;
+    cout << num1.sign() << endl;
+
+    cout << (num1 < num8) << endl;
+    cout << (num7 == num8) << endl; 
 
     cout << "Worked" << endl;
 
